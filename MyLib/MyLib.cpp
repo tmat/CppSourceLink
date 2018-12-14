b641a05085880c3f259de3ca268e4e67fcd6a052
Tomas Matousek <tomat@microsoft.com>
2018-12-13 18:46:33 -0800

    First commit

---
+++ MyLib/MyLib.cpp
@@ -0,0 +1,11 @@
+// MyLib.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "MyLib.h"
+#include <stdio.h>
+
+void DoSomeNativeStuff()
+{
+	printf("Hello from C++!\r\n");
+}
